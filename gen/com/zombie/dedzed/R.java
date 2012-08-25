702a2668b4b4c5ea5f618baecfe6c4eaebe27ce2
Aaron Zimmermann <aaron.zimmermann@sparcedge.com>
2012-08-25 10:59:57 -0400

    Its... CLEAN

--- gen/com/zombie/dedzed/R.java
+++ gen/com/zombie/dedzed/R.java
@@ -15,7 +15,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int buttonMap=0x7f070000;
+        public static final int buttonTrade=0x7f070002;
+        public static final int buttonWall=0x7f070001;
+        public static final int menu_settings=0x7f070003;
     }
     public static final class layout {
         public static final int activity_survival=0x7f030000;
@@ -25,8 +28,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int buttonMap=0x7f040005;
+        public static final int buttonTrade=0x7f040007;
+        public static final int buttonWall=0x7f040006;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int titleSurvival=0x7f040004;
         public static final int title_activity_survival=0x7f040003;
     }
     public static final class style {
